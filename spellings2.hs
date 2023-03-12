0382d957e96bb91ae9d529895445d775bac31fb7
blademaw <jackoli3@me.com>
2023-03-12 16:08:39 +1100

    Add ability to print matrix

--- spellings2.hs
+++ spellings2.hs
@@ -5,19 +5,20 @@ main :: IO ()
 main = do
     ((center:_):outer:_) <- getArgs
     wordList <- lines <$> readFile "out.txt"
-    outputWords center outer wordList
+    let valids = getValidWords center outer wordList
+    putStrLn $ outputWords center outer valids
+    displayMatrix (outputMatrixValids valids) (sort (nub (map head valids)))
 
 -- TODO: change this to a func that returns a string, print in main
-outputWords :: Char -> [Char] -> [String] -> IO ()
-outputWords center outer wordList =
-    putStrLn $
-        "Valid words are:\n"
-            ++ unlines (map ("\t" ++) valids)
-        ++
-        "\nPangrams are:\n"
-            ++ unlines (map (\ p -> "\t" ++ (if isPerfectPangram' p then "* " else "  ") ++ p) (filter (isPangram center outer) valids))
+-- outputWords :: Char -> [Char] -> [String] -> String
+outputWords center outer valids =
+    "Valid words are:\n"
+        ++ unlines (map ("\t" ++) valids)
+    ++
+    "\nPangrams are:\n"
+        ++ unlines (map (\ p -> "\t" ++ (if isPerfectPangram' p then "* " else "  ") ++ p) (filter (isPangram center outer) valids))
     where
-        valids = getValidWords center outer wordList
+        -- valids = getValidWords center outer wordList
         isPerfectPangram' = isPerfectPangram center outer
 
 -- outputMatrix :: Char -> [Char] -> [String] -> IO ()
@@ -31,7 +32,37 @@ outputMatrix center outer wordList =
         startValids letter = filter (\ x -> head x == letter)
         maxLen = maximum $ map length valids
 
+outputMatrixValids valids =
+    [ [ length (filter (\ x -> length x == len) (startValids letter valids)) | len <- [4..maxLen] ] | letter <- letters ]
+    where
+        letters = sort (nub (map head valids))
+        startValids letter = filter (\ x -> head x == letter)
+        maxLen = maximum $ map length valids
 
+testdisplay = displayMatrix [[2,0,1,2,0,0],[10,6,7,10,6,1],[1,1,0,1,0,0],[0,0,2,0,0,1],[1,1,0,1,0,0],[2,1,0,1,0,0]] "afgikn"
+
+displayMatrix :: [[Int]] -> [Char] -> IO ()
+displayMatrix matrix letters = do
+  let maxLen = 3 + length (head matrix)
+      pads dig = if length dig == 2 then " " else "  "
+      sepMatr = foldr (\a b -> a ++ pads a ++ b) ""
+      colLabels = map show [4..maxLen] ++ ["Σ"]
+      rowLabels = map (:[]) letters
+      header = "   " ++ sepMatr colLabels
+      footer = "Σ" ++ "  " ++ sepMatr ([show (sum [(matrix !! i) !! j | i <- [0 .. length matrix - 1]]) |
+                                          j <- [0 .. length (head matrix) - 1]])
+                           ++ show (sum (map sum matrix))
+      rowStrings = zipWith (\label row -> label ++ "  " ++ sepMatr (map show row) ++ show (sum row)) rowLabels matrix
+  putStrLn header
+  mapM_ putStrLn rowStrings
+  putStrLn footer
+
+
+-- purely for testing; 6 mar 2023
+testValids = getValidWords 'f' "laking"
+
+
+-- finding valids
 isValid :: Char -> [Char] -> String -> Bool
 isValid center outer word =
     center `elem` word && all (\ l -> l `elem` (center:outer)) word
@@ -39,6 +70,7 @@ isValid center outer word =
 getValidWords :: Char -> [Char] -> [String] -> [String]
 getValidWords center outer = filter (isValid center outer)
 
+-- pangrams
 isPangram :: Char -> [Char] -> String -> Bool
 isPangram center outer word = center `elem` word && sort (nub word) == sort (nub (center:outer))
 
